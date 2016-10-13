db98cebdb6d07d00577ec59e2c9b6ba9d83d1d19
YUKIZ0ME <820927200@qq.com>
2016-10-13 20:01:03 +0800

    Create BubbleSort.cpp

---
+++ DataSturcture/Sort/BubbleSort.cpp
@@ -0,0 +1,72 @@
+//冒泡排序
+
+#include <iostream>
+#include <cstring>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::swap;
+class Vector {
+private:
+    int size, length;
+    int * data;
+public:
+    Vector(int input_size) {
+        size = input_size;
+        length = 0;
+        data = new int[size];
+    }
+    ~Vector() {
+        delete[] data;
+    }
+    bool insert(int loc, int value) {
+        if (loc < 0 || loc > length) {
+            return false;
+        }
+        if (length >= size) {
+            return false;
+        }
+        for (int i = length; i > loc; --i) {
+            data[i] = data[i - 1];
+        }
+        data[loc] = value;
+        length++;
+        return true;
+    }
+    void print() {
+        for (int i = 0; i < length; ++i) {
+            if (i > 0) {
+                cout << " ";
+            }
+            cout << data[i];
+        }
+        cout << endl;
+    }
+    void sort() {
+        for(int i = 0; i < length - 1; i++){
+            bool swapped = false;
+            for(int j = 0; j < length - i - 1; j++){
+                if(data[j] > data[j + 1]){
+                    swap(data[j], data[j + 1]);
+                    swapped = true;
+                }
+            }
+            if(swapped == false){
+                break;
+            }
+        }
+    }
+};
+int main() {
+    int n;
+    cin >> n;
+    Vector arr(n);
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        arr.insert(i, x);
+    }
+    arr.sort();
+    arr.print();
+    return 0;
+}
