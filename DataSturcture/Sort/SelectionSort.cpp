9a537bb68a45b8579e9d90b4bf6bf5ad79c37377
YUKIZ0ME <820927200@qq.com>
2016-10-13 20:04:59 +0800

    Create SelectionSort.cpp

---
+++ DataSturcture/Sort/SelectionSort.cpp
@@ -0,0 +1,67 @@
+//选择排序
+
+#include <iostream>
+#include <cstring>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::swap;
+class Vector {
+private:
+    int size, length;
+    int * data;
+public:
+    Vector(int input_size) {
+        size = input_size;
+        length = 0;
+        data = new int[size];
+    }
+    ~Vector() {
+        delete[] data;
+    }
+    bool insert(int loc, int value) {
+        if (loc < 0 || loc > length) {
+            return false;
+        }
+        if (length >= size) {
+            return false;
+        }
+        for (int i = length; i > loc; --i) {
+            data[i] = data[i - 1];
+        }
+        data[loc] = value;
+        length++;
+        return true;
+    }
+    void print() {
+        for (int i = 0; i < length; ++i) {
+            if (i > 0) {
+                cout << " ";
+            }
+            cout << data[i];
+        }
+        cout << endl;
+    }
+    void sort() {
+        for(int i = 0; i < length - 1; i++){
+            for(int j = i + 1; j < length; j++){
+                if(data[i] > data[j]){
+                    swap(data[i],data[j]);
+                }
+            }
+        }
+    }
+};
+int main() {
+    int n;
+    cin >> n;
+    Vector arr(n);
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        arr.insert(i, x);
+    }
+    arr.sort();
+    arr.print();
+    return 0;
+}
